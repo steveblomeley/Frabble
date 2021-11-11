20f619404255059ec251d34aaa9fce77785f7137
steveblomeley <steveblomeley@yahoo.co.uk>
2021-11-11 15:36:59 +0000

    Apply linter suggestions

--- Frabble.hs
+++ Frabble.hs
@@ -36,16 +36,16 @@ import Frabble.Move
 
 fillRack :: Rack -> Bag -> IO (Rack,Bag)
 fillRack rack bag = do
-    ls <- randomPick bag (rackSize - (length rack))
+    ls <- randomPick bag (rackSize - length rack)
     return (rack ++ ls, bag `without` ls)
 
 getTilesPlaced :: Board -> Board -> Either String [LiveTile]
-getTilesPlaced bNew bOld = 
-    if length tilesPlaced < 1
+getTilesPlaced bNew bOld =
+    if null tilesPlaced
         then Prelude.Left  "You must place at least one tile from your rack"
         else Prelude.Right tilesPlaced
     where
-        tilesPlaced = bNew `without` bOld        
+        tilesPlaced = bNew `without` bOld
 
 checkCentreSquareUsed :: Board -> Either String Bool
 checkCentreSquareUsed b =
@@ -54,22 +54,22 @@ checkCentreSquareUsed b =
         Nothing -> Prelude.Left "First move of the game must use the centre square"
 
 checkJoinsExistingTiles :: Board -> Board -> Rack -> Rack -> Move -> Either String Bool
-checkJoinsExistingTiles bNew bOld rNew rOld (Move p a w) = 
+checkJoinsExistingTiles bNew bOld rNew rOld (Move p a w) =
     if existingTilesUsed || perpendicularWords
         then Prelude.Right True
         else Prelude.Left  "Play a word that joins up with existing tiles on the board"
     where
         existingTilesUsed  = length w > length (rOld `without` rNew)
         perpendicularWords = not (null (findXWords a bNew (bNew `without` bOld)))
-   
+
 validateWordPlacement :: Board -> Board -> Rack -> Rack -> Move -> Either String Bool
-validateWordPlacement bNew bOld rNew rOld (Move p a w) = 
+validateWordPlacement bNew bOld rNew rOld (Move p a w) =
     if firstWordOnBoard
         then checkCentreSquareUsed bNew
         else checkJoinsExistingTiles bNew bOld rNew rOld (Move p a w)
     where
         firstWordOnBoard = null bOld
-             
+
 validateMove :: Board -> Board -> Rack -> Rack -> Move -> Either String [LiveTile]
 validateMove bNew bOld rNew rOld move = do
     checkWordBoundaries bNew move
@@ -81,13 +81,15 @@ validateNewWords :: Dictionary -> Board -> Move -> [LiveTile] -> Either String [
 validateNewWords d b (Move p a w) newTiles =
     case checkDictionary d (wordsToStrings newWords) of
         []       -> Prelude.Right newWords
-        badWords -> Prelude.Left ("Not in dictionary: " ++ (unwords badWords))
+        badWords -> Prelude.Left ("Not in dictionary: " ++ unwords badWords)
     where
         newWord   = findWord a b p
         newXWords = findXWords a b newTiles
         newWords  = newWord : newXWords
 
 -- This does all the validation etc to parse a move, validate it, and apply to the board
+-- Could define a new ADT to represent a special result from a turn - either an error, 
+-- or a command (such as Quit, Redraw Board, etc)
 tryMakeMove :: Dictionary -> Board -> Rack -> String -> Either String (Board,Rack,Int)
 tryMakeMove d b r input = do
     move <- parseMove input
@@ -96,20 +98,20 @@ tryMakeMove d b r input = do
     newTiles <- validateMove b' b r' r move
     newWords <- validateNewWords d b' move newTiles
     let score = calculateScore b' newWords newTiles r r'
-    return (b',r',score) 
+    return (b',r',score)
 
 showRack :: Rack -> IO ()
-showRack r = putStrLn ("Your tiles are: " ++ (interleave r (repeat ' ')))
+showRack r = putStrLn ("Your tiles are: " ++ interleave r (repeat ' '))
 
 getMove :: Rack -> IO String
-getMove r = 
-    showRack r >> putStr "Enter next move (e.g. A1 Across WORD)\n> " >> getLine >>= return
+getMove r =
+    showRack r >> putStr "Enter next move (e.g. A1 Across WORD)\n> " >> getLine
 
 -- The main game loop - get, parse & validate a move, apply to the board, score it, refill
 -- the players rack - repeat
 play :: Dictionary -> Board -> Rack -> Bag -> IO ()
 play d b r ts = do
-        result <- (tryMakeMove d b r) <$> (getMove r)
+        result <- tryMakeMove d b r <$> getMove r
         case result of
             Prelude.Left  e         -> retryMove e
             Prelude.Right (b',r',s) -> nextMove b' r' s
@@ -117,7 +119,7 @@ play d b r ts = do
             retryMove e    = do putStrLn e
                                 play d b r ts
             nextMove b r s = do printBoard b bonuses
-                                putStrLn ("Move scored " ++ (show s))
+                                putStrLn ("Move scored " ++ show s)
                                 (r',ts') <- fillRack r ts
                                 play d b r' ts'
 
@@ -125,4 +127,5 @@ startGame :: IO ()
 startGame = do
     (rack,bag) <- fillRack [] fullBag
     dict <- getDictionary
+    printBoard [] bonuses
     play dict [] rack bag
